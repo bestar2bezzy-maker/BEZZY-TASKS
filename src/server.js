a255e4d4291cb4fa1645fc77486d7a1cfb2b5a90
bestar2bezzy-maker <bestar2bezzy@gmail.com>
2026-09-12 17:34:49 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,18 @@
 
+const { env } = require('./config/env');
+const app = require('./app');
+const { initDb } = require('./config/database');
+
+initDb();
+
+const server = app.listen(env.PORT, env.HOST, () => {
+  console.log(`Bezzy Tasks V33.2.7 listening on ${env.HOST}:${env.PORT}`);
+});
+
+const shutdown = (signal) => {
+  console.log(`${signal} received; shutting down`);
+  server.close(() => process.exit(0));
+};
+
+process.on('SIGTERM', () => shutdown('SIGTERM'));
+process.on('SIGINT', () => shutdown('SIGINT'));
